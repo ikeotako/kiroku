87334024107d967f3b12da6aff5b417535cc4724
ikeotako <6tyhnvg6kd@privaterelay.appleid.com>
2026-08-31 10:21:42 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,38 +1,12 @@
-var CACHE = "kiroku-v1";
-var FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
-
-self.addEventListener("install", function (e) {
-  e.waitUntil(
-    caches.open(CACHE)
-      .then(function (c) { return c.addAll(FILES); })
-      .catch(function () {})
-      .then(function () { return self.skipWaiting(); })
-  );
-});
-
-self.addEventListener("activate", function (e) {
-  e.waitUntil(
-    caches.keys().then(function (ks) {
-      return Promise.all(ks.map(function (k) { return k === CACHE ? null : caches.delete(k); }));
-    }).then(function () { return self.clients.claim(); })
-  );
-});
-
-self.addEventListener("fetch", function (e) {
-  if (e.request.method !== "GET") return;
-  if (e.request.mode === "navigate") {
-    e.respondWith(
-      fetch(e.request).then(function (resp) {
-        var copy = resp.clone();
-        caches.open(CACHE).then(function (c) { c.put("./index.html", copy); });
-        return resp;
-      }).catch(function () {
-        return caches.match("./index.html");
-      })
-    );
-    return;
-  }
-  e.respondWith(
-    caches.match(e.request).then(function (r) { return r || fetch(e.request); })
-  );
-});
+var CACHE="kiroku-v2";
+var FILES=["./","./index.html","./manifest.webmanifest","./icon-180.png","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(FILES);}).catch(function(){}).then(function(){return self.skipWaiting();}));});
+self.addEventListener("activate",function(e){
+  e.waitUntil(caches.keys().then(function(k){return Promise.all(k.map(function(x){return x===CACHE?null:caches.delete(x);}));}).then(function(){return self.clients.claim();}));});
+self.addEventListener("fetch",function(e){
+  if(e.request.method!=="GET")return;
+  if(e.request.mode==="navigate"){
+    e.respondWith(fetch(e.request).then(function(r){var c=r.clone();caches.open(CACHE).then(function(cc){cc.put("./index.html",c);});return r;})
+      .catch(function(){return caches.match("./index.html");}));return;}
+  e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request);}));});
